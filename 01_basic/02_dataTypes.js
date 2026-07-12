6260280fdb6038a6e0cdc7b6c7ae356c335de526
amandubey0605 <amandubey.rk@gmail.com>
2026-07-12 07:00:37 +0000

    data Types

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as new version.
+//alert("hello")  we are using node js , not browser it give error here
+console.log(3+3)
+
+let name="aman"
+
+//number=> 2 to the power 53
+//bigInt
+//string=""
+//boolean=true/fasle
+//null
+//undifined
+//symbol
+// non primitive- object(function,array)
+console.log(typeof name)
+console.log(typeof null) //object
+console.log(typeof undefined)  //undifined
+  
